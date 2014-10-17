5fc55be47ad1f906aee86ee781c2c1f9c8595646
Jim Plush <jiminoc@gmail.com>
2014-10-16 17:11:36 -0700

    Testing a bad commit to make sure the travis build fails

--- cmd/gotelweb/gotel.go
+++ cmd/gotelweb/gotel.go
@@ -18,7 +18,8 @@ func main() {
 	dbPass := flag.String("GOTEL_DB_PASSWORD", "", "DB Pass")
 	confPath := flag.String("GOTEL_CONFIG_PATH", "./gotel.gcfg", "config file path")
 	sysLogEnabled := flag.Bool("GOTEL_SYSLOG", false, "Use syslog for output logging")
-	flag.Parse()
+	flag.Pa
+	rse()
 	flagenv.Parse()
 
 	config := gotel.NewConfig(*confPath, *sysLogEnabled)
